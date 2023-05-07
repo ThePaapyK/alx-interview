1b1c3cdb8b9e646ecbd790c674610fff1d3ea885
Paa Kojo Effah Annan <pkeannan77@gmail.com>
2023-05-07 15:52:23 +0000

    God is good

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+"""interview preparations - minimum operations"""
+
+def min_operations(n):
+    """Returns the smallests number of operations (Copy All and Paste)
+    needed to result in exactly n H
+    characters in this file.
+
+    Args:
+    n (int): number of H characters desired
+
+    Returns: number of operations
+    """
+
+    i = 0
+    pasted_H = 1
+    clipboard = 0
+
+    while pasted_H < n:
+        if clipboard == 0:
+            clipboard = pasted_H
+            i += 1
+
+        if pasted_H == 1:
+            pasted_H += clipboard
+            i += 1
+            continue
+
+        remainder = n - pasted_H
+
+        if remainder < clipboard:
+            return 0
+
+        if remainder % pasted_H != 0:
+            pasted_H += clipboard
+            i += 1
+        else:
+            clipboard = pasted_H
+            pasted_H += clipboard
+            i += 2
+
+    if pasted_H == n:
+        return i
+    else:
+        return 0
+
