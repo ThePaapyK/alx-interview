53cf664d1f5f7c9264ad5f252a741577c92d3944
Paa Kojo Effah Annan <pkeannan77@gmail.com>
2023-05-07 16:27:11 +0000

    open 'em

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-""" Interview preparation - determines if all boxes 
+""" Interview preparation - determines if all boxes
 in a group can be unlocked"""
 
 
@@ -8,27 +8,27 @@ def canUnlockAll(boxes):
     """Determines if all boxes can be opened.
 
     Args:
-        boxes (List[List[int]]): a list of lists, where each inner list contains
+    boxes (List[List[int]]): a list of lists, where each inner list contains
             the keys that unlock other boxes.
 
     Returns:
         bool: True if all boxes can be opened, False otherwise.
     """
-    unopened_boxes = set(range(1, len(boxes)))
 
-    opened_boxes = set([0])
+    # Create a set containing all box indices except for the first one
+    locked_boxes = set(range(1, len(boxes)))
+    # Create a set containing the keys for the first box
+    keys = set(boxes[0])
 
+    # Loop until there are no more keys to try
+    while keys:
+        # Pop a key from the set of keys
+        key = keys.pop()
+        # If the key opens a locked box, add the keys from the unlocked box to the set of keys
+        if key in locked_boxes:
+            keys.update(boxes[key])
+            # Remove the opened box from the set of locked boxes
+            locked_boxes.remove(key)
 
-    while unopened_boxes:
-        found_new_key = False
-
-        for key in boxes[opened_boxes.pop()]:
-            if key in unopened_boxes:
-                opened_boxes.add(key)
-                unopened_boxes.remove(key)
-            found_new_key = True
-
-        if not found_new_key:
-            break
-
-    return len(unopened_boxes) == 0
+    # If there are no more locked boxes, return True
+    return len(locked_boxes) == 0
