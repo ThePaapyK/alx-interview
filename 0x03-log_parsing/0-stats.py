3bbe883ce9b7785c4dc5f8c90e81d18841743c16
Paa Kojo Annan <pkeannan77@gmail.com>
2023-05-28 00:26:15 +0000

    for

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+import sys
+from collections import defaultdict
+
+# Initialize variables
+total_size = 0
+status_codes = defaultdict(int)
+line_count = 0
+
+try:
+    # Read input lines from stdin
+    for line in sys.stdin:
+        line = line.strip()
+        # Check if the line matches the expected input format
+        if line.startswith("[") and line.endswith("]"):
+            parts = line.split()
+            ip_address = parts[0]
+            status_code = parts[-2]
+            file_size = parts[-1]
+
+            # Update metrics
+            total_size += int(file_size)
+            status_codes[status_code] += 1
+            line_count += 1
+
+        # Check if we reached 10 lines or a keyboard interruption (CTRL + C)
+        if line_count == 10:
+            # Print metrics
+            print("Total file size:", total_size)
+            for code in sorted(status_codes.keys()):
+                print(code + ":", status_codes[code])
+            print()
+
+            # Reset variables
+            status_codes = defaultdict(int)
+            line_count = 0
+
+except KeyboardInterrupt:
+    # If interrupted, print the metrics for the processed lines
+    print("Total file size:", total_size)
+    for code in sorted(status_codes.keys()):
+        print(code + ":", status_codes[code])
+
