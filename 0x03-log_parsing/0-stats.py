48a521e493cc5cc001a5dc921ccceae0f4a89108
Paa Kojo Annan <pkeannan77@gmail.com>
2023-06-17 00:12:01 +0000

    Odogwu

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,83 +1,56 @@
 #!/usr/bin/python3
 
-"""
-This module provides functionality to compute and
-print metrics based on input data.
+"""Script that reads stdin line by line and computes metrics"""
 
-It reads input from stdin line by line,
-computes statistics, and prints the results.
-
-Input format:
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-(if the format is not this one, the line must be skipped)
-
-After every 10 lines and/or a keyboard interruption (CTRL + C),
-it prints the following statistics:
-- Total file size: the sum of all previous file sizes
-- Number of lines by status code: counts the occurrences of each status code
-
-Possible status codes: 200, 301, 400, 401, 403, 404, 405, and 500
-
-The module uses the `print_stats()` function to print the statistics and
-a dictionary `status_codes` to keep track of the counts for each status code.
-
-Usage:
-    $ python3 <module_name.py>
-
-Author: Paa Kojo Effah Annan
-"""
 import sys
-from collections import defaultdict
 
 
-def print_stats(status_dict, total_size):
-    """Prints the computed statistics.
+def print_metrics(metrics_dict, total_size):
+    """
+    Prints the metrics information.
 
     Args:
-        status_dict (dict): Dictionary containing
-        the count of lines per status code.
+        metrics_dict (dict): A dictionary containing
+        the metrics as key-value pairs.
+        total_size (int): The total file size.
 
-        total_size (int): Total file size computed from the input.
-
-     Prints:
-        File size: Total size of all files processed.
-        <status code>: Number of lines with the respective status code.
+    Returns:
+        None
     """
-    print(f"File size: {total_size}")
-    for status_code, count in sorted(status_dict.items()):
-        if count != 0:
-            print(f"{status_code}: {count}")
+    print("File size: {:d}".format(total_size))
+    for key in sorted(metrics_dict.keys()):
+        if metrics_dict[key] != 0:
+            print("{}: {:d}".format(key, metrics_dict[key]))
 
 
-status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-                "404": 0, "405": 0, "500": 0}
+metrics = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+           "404": 0, "405": 0, "500": 0}
 
-count = 0
+line_count = 0
 total_size = 0
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            print_stats(status_codes, total_size)
+        line_count += 1
 
-        parts = line.split()
-        count += 1
+        if line_count != 0 and line_count % 10 == 0:
+            print_metrics(metrics, total_size)
+
+        line_split = line.split()
 
         try:
-            file_size = int(parts[-1])
-            total_size += file_size
+            total_size += int(line_split[-1])
         except ValueError:
             pass
 
         try:
-            status_code = parts[-2]
-            if status_code in status_codes:
-                status_codes[status_code] += 1
+            if line_split[-2] in metrics:
+                metrics[line_split[-2]] += 1
         except IndexError:
             pass
 
-    print_stats(status_codes, total_size)
+    print_metrics(metrics, total_size)
 
 except KeyboardInterrupt:
-    print_stats(status_codes, total_size)
+    print_metrics(metrics, total_size)
     raise
